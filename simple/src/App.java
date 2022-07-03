5961376a098f28107a6efe52c84dee5530ecefca
ampinkus <ampinkus@gmail.com>
2022-07-03 20:20:41 -0300

    Commit from Windows 10

--- simple/src/App.java
+++ simple/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         System.out.println("Hola Alfredo commit 2");
+        System.out.println("Hola Alfredo commit from Windows 10");
     }
 }
