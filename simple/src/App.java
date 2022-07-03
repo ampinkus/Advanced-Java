bd4710769efb09f1f4e2f525728af242889226ec
Alfredo Pinkus <ampinkus@gmail.com>
2022-07-03 15:50:13 -0300

    Actualizacion en main

--- simple/src/App.java
+++ simple/src/App.java
@@ -1,10 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
-<<<<<<< HEAD
         System.out.println("Hola Alfredo desde el branch Master");
-=======
-        System.out.println("Hola Alfredo on test Branch");
->>>>>>> Alfredo
     }
 }
